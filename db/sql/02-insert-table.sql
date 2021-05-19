79beefd88614febb456c8151ea67c4e534a6f28e
bearddan2000 <bearddan2000@yahoo.com>
2021-05-18 22:57:19 -0400

    initial commit

---
+++ db/sql/02-insert-table.sql
@@ -0,0 +1,8 @@
+TRUNCATE `beverage`.pop;
+
+INSERT INTO `beverage`.pop (id, name, color)
+VALUES
+(default, 'Am Bulldog', 'White'),
+(default, 'Blue Tick', 'Grey'),
+(default, 'Labrador', 'Black'),
+(default, 'Gr Shepard', 'Brown');
