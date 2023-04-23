6d87a8cb8b3b82ff66bf8acf09ab80277beb6dca
bearddan2000 <bearddan2000@yahoo.com>
2023-04-22 20:04:26 -0400

    initial commit

---
+++ py-test-srv/bin/const.py
@@ -0,0 +1,28 @@
+URL = 'http://py-srv:5000'
+
+GET_ALL_URL = URL + '/pop'
+
+STATIC = {
+  "pop": [
+    {
+      "color": "brown",
+      "name": "RC Cola"
+    },
+    {
+      "color": "clear",
+      "name": "Sprite"
+    },
+    {
+      "color": "brown",
+      "name": "Verners"
+    },
+    {
+      "color": "green",
+      "name": "Mt. Lightening"
+    }
+  ]
+}
+
+SMOKE_URL = URL + '/'
+
+SMOKE = {"hello": "world"}
